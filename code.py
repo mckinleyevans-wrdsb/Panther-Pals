2d420f4a2498aeecc448d18ef18c4998b4028778
mckinleyevans-wrdsb <130684272+mckinleyevans-wrdsb@users.noreply.github.com>
2023-05-04 02:15:30 +0000

    Initial commit

---
+++ code.py
@@ -0,0 +1,12 @@
+def attach_content_to_element(id, content):
+  Element(id).element.innerHTML = Element(id).element.innerHTML + content
+
+def build_element(type='div', class_name='', id='', content=''):
+  class_string = f' class={class_name}' if class_name else ''
+  id_string = f' id={id}' if id else ''
+  return f'''
+    <{type}{class_string}{id_string}>
+      {content}
+    </{type}>
+  '''
+
