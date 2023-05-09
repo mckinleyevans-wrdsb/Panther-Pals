a5e61ed5e616f89a7a134abf32b0522921152f27
Qurea2166 <130780706+Qurea2166@users.noreply.github.com>
2023-05-09 16:05:08 +0000

    Implement code.py functions into calendar

--- calendar.py
+++ calendar.py
@@ -1,16 +1,6 @@
 import datetime
+from code import build_element, attach_content_to_element
 
-# output content to the given element
-def output_content(id, content):
-  Element(id).element.innerHTML += content
-
-# make element from given variables
-def make_element(element_type, element_class, element_id, element_content):
-  return f'''
-  <{element_type} class='{element_class}' id='{element_id}'>
-    {element_content}
-  </{element_type}>
-  '''
 # get the amount of days in a given month
 def get_days_in_month(month, is_leap_year):
   month_days = 0
@@ -41,7 +31,7 @@ columns = 5
 def make_day():
   day_content = f'''
   <div class='calendar-day'>
-    <h4>'#'</h4>
+    <h4>#</h4>
   </div>
   '''
   return day_content
@@ -70,5 +60,12 @@ for date in date_iterate(2023, 5):
   pass
   
 
-output_content('pyscript-calendar', make_element(element_type='div', element_class='calendar', element_id='calendar', element_content=calendar_content))
+attach_content_to_element('pyscript-calendar', 
+               build_element(
+                 type='div', 
+                 class_name='calendar',
+                 id='calendar', 
+                 content=calendar_content
+               )
+              )
 
