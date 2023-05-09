fe621b897f5cd2ee3f8cc96299ad81dd67656767
rileycassidy <130694695+rileycassidy@users.noreply.github.com>
2023-05-09 16:44:16 +0000

    Initial Commit

---
+++ calendar.py
@@ -0,0 +1,7 @@
+class calender_info:
+  def __init__ (self, name, date, startTime, endTime, eventClass=None)
+  self._name = name 
+  self._date = date
+  self._startTime = startTime
+  self._endTime = endTime
+  self.eventClass = eventClass
---
+++ users.py
@@ -0,0 +1,7 @@
+class user:
+  ## Level would be admin status
+ def __init__(self, name, age, isTeacher=False, level=1, )
+  self._name = name
+  self._age = age
+  self._isTeacher = isTeacher
+  self._level = level
