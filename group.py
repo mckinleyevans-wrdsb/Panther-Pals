a7eab7547db2d3bb0160fe60b3f075eab3094068
N0-N <132595720+N0-N@users.noreply.github.com>
2023-05-11 16:55:05 +0000

    Abeer - class - group

---
+++ group.py
@@ -0,0 +1,11 @@
+#Class Group:
+# - Attributes
+# - name
+# - grade
+# - userList
+class group:
+ 
+  def __init__(self, name, grade, userList)
+  self.name = name
+  self.grade = grade
+  self.userList = userList
