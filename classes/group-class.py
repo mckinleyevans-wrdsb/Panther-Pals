f435e459f9cf56b1a8f5ca7463eee85fd1f07349
rileycassidy <130694695+rileycassidy@users.noreply.github.com>
2023-05-15 16:10:34 +0000

    revision of Group Class

--- classes/group-class.py
+++ classes/group-class.py
@@ -1,23 +1,30 @@
+import users
+
 #Class Group:
 # - Attributes
 # - name
-# - grade
-# - userList
-class group:
-  def __init__(self, name, grade, userIDs)
-  self._name = name
-  self._grade = grade
-  self._userIDs = userIDs
-  return self
+# - list of users
+# - Total numbers of users to be added to group
+# - Is the group active
+# - id (used from user) for staff
+class group():
+  #constructor
+  def __init__(self, groupName, userList, grouptTotal, isActive = True, staff_id)
+    self._groupName = groupName
+    self._userList = userList
+    self._groupTotal = groupTotal
+    self._isActive = isActive
+    self._staff_sponsor_id = staff_id  
+    return self
 
 class classroom:
-  def __init__(self, name, grade, userIDs, teacher)
-  self._teacher = teacher
-  group.__init__(self, name, grade, userIDs)
+  def __init__(self, groupName, userList, grouptTotal, isActive = True, staff_id, department)
+  self._department = department
+  group.__init__(self, groupName, userList, grouptTotal, isActive = True, staff_id)
   return self
 
 class club:
-  def __init__(self, name, grade, userIDs, teacher)
-  self._teacher = teacher
-  group.__init__(self, name, grade, userIDs)
+  def __init__(self, groupName, userList, grouptTotal, isActive = True, staff_id, clubType)
+  self._clubType = clubType
+  group.__init__(self, groupName, userList, grouptTotal, isActive = True, staff_id)
   return self
