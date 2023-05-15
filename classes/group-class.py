cf037f9c3a7ad9e1a2ff1054dd782dd94593a71c
N0-N <132595720+N0-N@users.noreply.github.com>
2023-05-15 18:04:08 +0000

    Abeer - Fixing of some variable names

--- classes/group-class.py
+++ classes/group-class.py
@@ -9,22 +9,22 @@ import users
 # - id (used from user) for staff
 class group():
   #constructor
-  def __init__(self, group_name, user_list, groupt_total, is_active = True, staff_id)
+  def __init__(self, group_name, user_list, group_total, is_active = True, staff_id)
     self._group_name = group_nameame
-    self._user_list = user_listist
+    self._user_list = user_list
     self._group_total = group_total
     self._is_active = is_active
     self._staff_sponsor_id = staff_id  
     return self
 
 class classroom:
-  def __init__(self, group_name, user_list, groupt_total, is_active = True, staff_id, department)
+  def __init__(self, group_name, user_list, group_total, is_active = True, staff_id, department)
   self._department = department
-  group.__init__(self, group_name, user_list, groupt_total, is_active = True, staff_id)
+  group.__init__(self, group_name, user_list, group_total, is_active = True, staff_id)
   return self
 
 class club:
-  def __init__(self, group_name, user_list, groupt_total, is_active = True, staff_id, club_type)
+  def __init__(self, group_name, user_list, group_total, is_active = True, staff_id, club_type)
   self._club_type = club_type
-  group.__init__self, group_name, user_list, groupt_total, is_active = True, staff_id)
+  group.__init__self, group_name, user_list, group_total, is_active = True, staff_id)
   return self
