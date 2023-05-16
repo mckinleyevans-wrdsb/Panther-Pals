9d1f3deb9683e4f01e494973fdebbdab2a5fed6b
TayyabAhmed561 <130687390+TayyabAhmed561@users.noreply.github.com>
2023-05-16 16:47:03 +0000

    final changes - Tayyab Ahmed

--- classes/notifications.py
+++ classes/notifications.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-class Notification:
-=======
 class Notification():
->>>>>>> a97f0261d8e894b5ec1432214757369ad84603b7
   def __init__ (self, title, text, description):
     self._title = title
     self._description = description
--- classes/users.py
+++ classes/users.py
@@ -1,27 +1,19 @@
-<<<<<<< HEAD
 class User:
  def __init__(self, id, name, pronouns):
     self._id = id
     self._name = name
     self._pronouns = pronouns
-=======
-class user:
- def __init__(self, id, name, pronouns)
-  self._id = id
-  self._name = name
-  self._pronouns = pronouns
->>>>>>> a97f0261d8e894b5ec1432214757369ad84603b7
-  return self
+    return self
 
 class Teacher(user):
   def __init__(self, id, name, pronouns, department):
     self._department = department
   user.__init__(self, id, name, pronouns)
-  return self
+    return self
 
 class Student(user):
   def __init__(self, id, name, pronouns, grade, age):
     self._grade = grade
     self._age = age
   user.__init__(self, id, name, pronouns)
-  return self
+    return self
