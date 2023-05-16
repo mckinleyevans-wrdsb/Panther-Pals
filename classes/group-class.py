a97f0261d8e894b5ec1432214757369ad84603b7
N0-N <132595720+N0-N@users.noreply.github.com>
2023-05-16 16:15:08 +0000

    Simply to sync

--- classes/group-class.py
+++ classes/group-class.py
@@ -10,7 +10,7 @@ import uuid
 # - id (used from user) for staff
 class group():
   #constructor
-  def __init__(self, group_name, user_list, group_total, is_active = True, uuid.uuid4())
+  def __init__(self, group_name, user_list, group_total, is_active = True, uuid):
     self._group_name = group_nameame
     self._user_list = user_list
     self._group_total = group_total
@@ -19,13 +19,13 @@ class group():
     return self
 
 class classroom:
-  def __init__(self, group_name, user_list, group_total, is_active = True, uuid.uuid4()), department)
-  self._department = department
-  group.__init__(self, group_name, user_list, group_total, is_active = True, uuid.uuid4()))
-  return self
+  def __init__(self, group_name, user_list, group_total, is_active = True, uuid, department):
+    self._department = department
+    group.__init__(self, group_name, user_list, group_total, is_active = True, uuid.uuid4())):
+    return self
 
 class club:
-  def __init__(self, group_name, user_list, group_total, is_active = True, , club_type)
-  self._club_type = club_type
-  group.__init__self, group_name, user_list, group_total, is_active = True, )
-  return self
+  def __init__(self, group_name, user_list, group_total, is_active = True, uuid, club_type):
+    self._club_type = club_type
+    group.__init__self, group_name, user_list, group_total, is_active = True):
+    return self
