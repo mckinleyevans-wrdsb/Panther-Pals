5b0b6afca4572cc4c8356cf6740792b63323001b
N0-N <132595720+N0-N@users.noreply.github.com>
2023-05-17 16:04:32 +0000

    Abeer - Calendar post fix

--- classes/calendar.py
+++ classes/calendar.py
@@ -1,9 +1,16 @@
 class Calender():
-  def __init__ (self, calender_posts, text, date):
+  def __init__ (self, calender_posts, event):
     self._uuid = uuid.uuid4()
-    self._calender_posts = calender_posts
+    self._calender_post = calender_post
+    self._event = event
+    return self
+
+class Calendar_post(Calendar):
+  def __init__(self, event, text, date):
     self._text = text
     self._date = date
+    Calendar.__init__(self, Calendar_post, event)
     return self
+    
   
 
