a0a122748c49735a4be6ec42b18099ed3585e618
mckinleyevans-wrdsb <130684272+mckinleyevans-wrdsb@users.noreply.github.com>
2023-05-19 13:30:54 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// https://www.w3schools.com/js/js_window_location.asp
-// https://docs.pyscript.net/latest/guides/passing-objects.html
-
-console.log('!!!!!!')
-
-
-
-var current_pathname = window.location.pathname;
