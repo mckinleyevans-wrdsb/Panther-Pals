e4307e340c2271b206fbf4a1771d2181ca1afc4d
Qurea2166 <130780706+Qurea2166@users.noreply.github.com>
2023-05-26 16:18:56 +0000

    Add build announcement function to announcements.py

--- announcements/announcements.py
+++ announcements/announcements.py
@@ -1,8 +1,40 @@
 import domControl
 
+# builds an announcement and adds it to the html
+def build_announcement():
 
-domControl.build_element('div', 'announcement','announcement')
+  announcement_content = '''
+  <div class = 'announcement'>
+    <h4>
+      Announcement Title
+    </h4>
+    
+    <p>
+      This is the announcement description.
+    </p>
+  </div>
+  '''
 
-announcement = input('What announcment do you want to make')
+  announcement_box_content = f'''
+  <div>
+    {announcement_content}
+  <div>
+  '''
+  
+  domControl.attach_content_to_element('pyscript-announcements', domControl.build_element(
+  type='div',
+  class_name='announcement-box',
+  id='announcement', 
+  content=announcement_box_content,))
+  
 
-domControl.attach_content_to_element('pyscript-announcements','<h4>  </h4>'+announcement)
+# amount of announcements currently
+num_of_announcements = 3
+
+# loop and make an announcement for each
+for i in range(num_of_announcements):
+  build_announcement()
+  
+
+#announcement = input('What announcment do you want to make')
+
