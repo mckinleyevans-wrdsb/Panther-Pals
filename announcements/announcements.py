3dba706ae0821ab8406464ed48941f49d9510434
Qurea2166 <130780706+Qurea2166@users.noreply.github.com>
2023-06-05 16:29:03 +0000

    Comment announcenments.py

--- announcements/announcements.py
+++ announcements/announcements.py
@@ -3,6 +3,7 @@ import crud_announcement
 # builds an announcement and adds it to the html
 def build_announcement(list, index):
 
+  # read from the list at index
   announcement_content = f'''
   <div class = 'announcement'>
     <h4>
@@ -18,20 +19,16 @@ def build_announcement(list, index):
   return announcement_content
   
 
-
+# get announcement list from announcement crud
 announcement_list = crud_announcement.get_existing_data("announcement.json")
 
-
-
-
-# amount of announcements currently
+# amount of announcements based on lenght of list
 num_of_announcements = len(announcement_list)
 
 announcement_box_content = "<div class = 'announcement-box'>"
 
 # loop and make an announcement for each
 for i in range(num_of_announcements):
-
   announcement_box_content += build_announcement(announcement_list, i)
   
 # build announcement box
