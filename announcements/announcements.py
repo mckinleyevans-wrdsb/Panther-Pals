3166760046721b3d5755f8a1489c30963e6792e2
Qurea2166 <130780706+Qurea2166@users.noreply.github.com>
2023-06-05 16:22:43 +0000

    Add announcement mock data info as the content for each announcement box

--- announcements/announcements.py
+++ announcements/announcements.py
@@ -1,16 +1,16 @@
 import domControl
 import crud_announcement
 # builds an announcement and adds it to the html
-def build_announcement():
+def build_announcement(list, index):
 
-  announcement_content = '''
+  announcement_content = f'''
   <div class = 'announcement'>
     <h4>
-      Announcement Title
+      {list[index]["_title"]}
     </h4>
     
     <p>
-      This is the announcement description.
+      {list[index]["_text"]}
     </p>
   </div>
   '''
@@ -19,20 +19,20 @@ def build_announcement():
   
 
 
-crud_announcement.get_existing_data('announcement.json')
+announcement_list = crud_announcement.get_existing_data("announcement.json")
 
 
 
 
 # amount of announcements currently
-num_of_announcements = 5
+num_of_announcements = len(announcement_list)
 
 announcement_box_content = "<div class = 'announcement-box'>"
 
 # loop and make an announcement for each
 for i in range(num_of_announcements):
 
-  announcement_box_content += build_announcement()
+  announcement_box_content += build_announcement(announcement_list, i)
   
 # build announcement box
 domControl.attach_content_to_element('pyscript-announcements', domControl.build_element(
